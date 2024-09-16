fc6a51f8fc059c062b5e29ef3601bedc6a8bb28b
Anita Pahangdar <anita.pahangdar@gmail.com>
2024-09-16 04:43:27 -0400

    input from user

---
+++ index.py
@@ -0,0 +1,11 @@
+# Step 1: Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("+   For addition")
+print("-   For subtraction")
+print("*   For multiplication")
+print("/   For division")
+
+operation = input("Enter the operation (+, -, *, /): ")
+
